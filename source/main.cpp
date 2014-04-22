0c5bc6b94fed336df54683f82836508b8cc2b0f0
Rafal Przywarski <rafal.przywarski@gmail.com>
2014-04-22 20:27:35 +0200

    refactored main

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,74 @@
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
+#include <functional>
 
-int main()
+namespace cell
+{
+
+struct configuration
+{
+    std::string project_name;
+};
+
+struct configuration_error : std::runtime_error
+{
+    configuration_error(const std::string& message) : std::runtime_error(message) { }
+};
+
+configuration load_configuration()
 {
     std::ifstream project("build.cell");
     if (!project.is_open())
+        throw configuration_error("build.cell not found");
+
+    std::string dummy;
+    configuration configuration;
+    project >> dummy >> configuration.project_name;
+    return std::move(configuration);
+}
+
+void build_targets(const configuration& configuration, std::function<void(const std::string& )> compile, std::function<void(const std::string& )> link)
+{
+    compile("*.cpp");
+    link(configuration.project_name);
+}
+
+void compile_cpp(const std::string& cpp)
+{
+    auto compile_cmd = "g++ -c " + cpp;
+    std::system(compile_cmd.c_str());
+}
+
+void link_target(const std::string& target)
+{
+    auto link_cmd = "g++ *.o -o " + target;
+    std::system(link_cmd.c_str());
+}
+
+void build_targets(const configuration& configuration)
+{
+    build_targets(configuration, compile_cpp, link_target);
+}
+
+void run()
+{
+    auto configuration = load_configuration();
+    build_targets(configuration);
+}
+
+}
+
+int main()
+{
+    try
     {
-        std::cerr << "build.cell not found" << std::endl;
+        cell::run();
+    }
+    catch (const cell::configuration_error& error)
+    {
+        std::cerr << error.what() << std::endl;
         return 1;
     }
-
-    std::string dummy, project_name;
-    project >> dummy >> project_name;
-
-    auto cmd = "g++ -c *.cpp && g++ *.o -o " + project_name;
-
-    std::system(cmd.c_str());
-
     return 0;
 }
